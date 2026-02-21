c06947dbcf30bf046b623694f9962ec80d6d6053
DrCharlatanLinuxK <sahsasimonenko@gmail.com>
2026-02-21 15:27:19 +0300

    Try linux pull

--- DRCFooBar/PullList.cs
+++ DRCFooBar/PullList.cs
@@ -30,15 +30,85 @@ namespace DRCFLCClient
         private async Task<(bool Ok, string? err)> AddTrackAsync(string name)
         {
             var args = string.Join(" ", PullNames.Select(f => $"\"{f}\""));
-            
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) { //start whis foobar Windows
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                //start whis foobar Windows
                 Console.WriteLine("Pushing");
-                Process.Start(new ProcessStartInfo { FileName = @"C:\Program Files\foobar2000\foobar2000.exe", Arguments = args, UseShellExecute = true });
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = @"C:\Program Files\foobar2000\foobar2000.exe", Arguments = args, UseShellExecute = true
+                });
                 Console.WriteLine("End Push");
-            } 
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) { Console.WriteLine("Linux no avaible"); }
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                #error Not Work Dot use
+                try
+                {
+                    if (!File.Exists(name))
+                        return (false, $"File not found: {name}");
+
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        // Windows: foobar2000
+                        var startInfo = new ProcessStartInfo
+                        {
+                            FileName = @"C:\Program Files\foobar2000\foobar2000.exe",
+                            UseShellExecute = true
+                        };
+                        // foobar принимает файлы просто как аргументы
+                        startInfo.ArgumentList.Add(name);
+
+                        Process.Start(startInfo);
+                        Console.WriteLine($"[Windows] Pushed to foobar2000: {name}");
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        // Linux: VLC
 
 
+                        var startInfo = new ProcessStartInfo
+                        {
+                            FileName = "vlc",
+                            UseShellExecute = false
+                        };
+
+                        // Только ArgumentList - никаких Arguments!
+                        startInfo.ArgumentList.Add("--playlist-enqueue");
+                        startInfo.ArgumentList.Add("--no-video-title-show");
+                        startInfo.ArgumentList.Add(name); // Путь к файлу добавится корректно, даже с пробелами
+
+                        using var process = Process.Start(startInfo);
+                        Console.WriteLine($"[Linux] Enqueued to VLC: {name}");
+                    }
+
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error starting player: {ex.Message}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return (false, ex.Message);
+                }
+
+
+                // xdg-open linux 
+                /*else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    // Откроет файл в плеере ПО УМОЛЧАНИЮ в системе пользователя
+                    var startInfo = new ProcessStartInfo
+                    {
+                        FileName = "xdg-open",
+                        Arguments = $"\"{name}\"",
+                        UseShellExecute = false
+                    };
+                    Process.Start(startInfo);
+                }*/
+
+                
+            }
             return (true, null);
         }
 
